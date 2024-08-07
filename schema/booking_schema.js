aaf5ca86e970d7bc674befe5f594c83dc34f5b79
Felix Kojo <felixkwadwo300@gmail.com>
2024-08-07 15:48:23 +0000

    endPoints v1.3

--- schema/booking_schema.js
+++ schema/booking_schema.js
@@ -1,16 +1,6 @@
 import Joi from "joi";
 
 export const bookingSchema = Joi.object({
-  departureCity: Joi.string().required().max(255),
-
-  arrivalCity: Joi.string().required().max(255),
-
-  departureTime: Joi.string().required().max(255),
-
-  arrivalTime: Joi.string().required().max(255),
-
-  date: Joi.date(),
-
   paymentMethod: Joi.string().valid("momo", "visa"),
 
   dependants: Joi.array().items(
